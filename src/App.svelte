67c674abe1d6286501fbc4a5c20412f784b99994
kuchromik <kai.chromik@online.de>
2023-01-02 18:39:21 +0100

    UTB Init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+</script>
+<Header />
+<main>
+	<h1>Ufftragsbuch-Online</h1>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dignissimos placeat ducimus possimus, modi dolorem molestias ea explicabo totam architecto excepturi reiciendis similique odit doloribus consequatur quasi exercitationem! Perspiciatis, excepturi?</p>
+</main>
+<Footer />
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<header>
+    <h1>
+        <img src="/img/_logo_big.png" alt="Chromik Offsetdruck">
+    </h1>
+</header>
+
+<style>
+
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+</style>
