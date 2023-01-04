a8b6960aad193275c69e4b38ab3f4cad8266bb9c
kuchromik <kai.chromik@online.de>
2023-01-04 06:56:47 +0100

    changed 03.01.2023

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,35 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
 
 	//tabs
 	let items = ['Current Jobs', 'Add New Job'];
 	let activeItem = 'Current Jobs';
 
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
+
 </script>
 <Header />
 <main>
 	<h1>Ufftragsbuch-Online</h1>
-	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dignissimos placeat ducimus possimus, modi dolorem molestias ea explicabo totam architecto excepturi reiciendis similique odit doloribus consequatur quasi exercitationem! Perspiciatis, excepturi?</p>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Jobs'}
+		<p>Joblist comes here</p>
+	{:else if activeItem === 'Add New Job'}
+		<p>Create New Job comes here</p>
+	{/if}
 </main>
 <Footer />
 <style>
 	main {
 		max-width: 960px;
 		margin: 40px auto;
+	}
+	h1{
+		text-align: center;
+		color: #555;
 	}
 </style>
