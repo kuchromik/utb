985692baac27e426c0046b0d38412aa511605c4e
kuchromik <kai.chromik@online.de>
2023-02-01 20:15:52 +0100

    01.02.2023 2

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,9 @@
 	import JobList from "./components/JobList.svelte";
 	import Archiv from "./components/Archiv.svelte";
 	import CreateJobForm from "./components/CreateJobForm.svelte";
-	import SignIn from "./components/SignIn.svelte";
 	import Tabs from "./shared/Tabs.svelte";
+	import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import Button from './shared/Button.svelte';
 
 
 	//tabs
@@ -22,10 +23,48 @@
 
 	}
 
+	
+
+    let email = '';
+    let password = '';
+
+    
+    const login = () => {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+  	  .then((userCredential) => {
+      
+      // Signed in 
+      const user = userCredential.user;
+      })
+      .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+      });
+    }
+  
+
 </script>
 <Header />
+
 <main>
-	<SignIn />
+	<div>
+		<form on:submit|preventDefault={login}>
+			<div class="form-field">
+				<label for="email">E-Mail</label>
+				<input type="email" id="email" bind:value={email}>
+			   
+			</div>
+			<div class="form-field">
+				<label for="password">Passwort</label>
+				<input type="password" id="password"bind:value={password}>
+				
+			</div>
+			<Button type="secondary" flat={false}>LOGIN</Button>
+		</form>
+	</div>
+
 	<h1>Ufftragsbuch-Online</h1>
 	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
 	{#if activeItem === 'Aktuell'}
@@ -46,4 +85,26 @@
 		text-align: center;
 		color: #555;
 	}
+	form{
+      width: 600px;
+      margin: 0 auto;
+      text-align: center;
+  }
+  .form-field{
+      margin: 18px auto;
+      border: solid 2px crimson;
+      background-color: lightgray;
+      border-radius: 6px;
+  }
+  input {
+      width: 95%;
+      border-radius: 6px;
+      margin: 10px;
+  }
+
+  label{
+      margin: 10px;
+      text-align: left;
+  }
+ 
 </style>
