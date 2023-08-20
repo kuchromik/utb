de444bfad7aaed451796353d1d638c8465f5c817
Kai-Uwe Chromik <kai.chromik@online.de>
2023-08-20 16:25:07 +0200

    improve Auswertung

--- src/components/Auswertung.svelte
+++ src/components/Auswertung.svelte
@@ -1,45 +1,31 @@
 <script>
 
+    import { fade } from 'svelte/transition';
+
     import {
     getFirestore, collection, onSnapshot, doc, getDocs,
-    query, where, orderBy
+    query, where, orderBy, waitForPendingWrites
+
     } from 'firebase/firestore';
 
     const db = getFirestore()
 
     let jobList = [];
 
+    let loopdelay = 0;
+
     let producerSumlist = [{
-     name: "chr",
-     sum: 0
+        id: 0,
+        name: "chr",
+        sum: 0
     }];
 
-    let index;
-
-    let summe_chr = 0;
-    let summe_doe = 0;
-    let summe_pwd = 0;
-    let summe_wmd = 0;
-    let summe_sax = 0;
-    let summe_sil = 0;
-    let summe_pap = 0;
-    let summe_prm = 0;
-    let summe_hee = 0;
 
     const colRef = query(collection(db, "Jobs"));
 
 
     const unsubscribe = onSnapshot(colRef, querysnapshot => {
                 let fbJobs = [];
-                let sum_chr = 0;
-                let sum_doe = 0;
-                let sum_pwd = 0;
-                let sum_wmd = 0;
-                let sum_sax = 0;
-                let sum_sil = 0;
-                let sum_pap = 0;
-                let sum_prm = 0;
-                let sum_hee = 0;
 
                     querysnapshot.forEach((doc) => {
                         let job = { ...doc.data(), id: doc.id};
@@ -51,90 +37,74 @@
 
 
                         if (!producerExist) {
-                            producerSumlist.push({name: job.producer, sum: 0})
-                        }
+                            let newIndex = producerSumlist.length;
+                            producerSumlist.push({id: newIndex, name: job.producer, sum: 0})
+                            }
 
-                        index = producerSumlist.findIndex(producer => producer.name === job.producer);
-
-                        console.log(index);
+                        let index = producerSumlist.findIndex(producer => producer.name === job.producer);
 
                         producerSumlist[index].sum = producerSumlist[index].sum + Number(job.amount);
 
-                        /* console.log(producerSumlist[index].name + producerSumlist[index].sum); */
 
-                
-                        switch (job.producer) {
-                            case "chr":
-                            sum_chr = sum_chr + Number(job.amount);
-                            break;
-                            case "doe":
-                            sum_doe = sum_doe + Number(job.amount);
-                            break;
-                            case "pwd":
-                            sum_pwd = sum_pwd + Number(job.amount);
-                            break;
-                            case "wmd":
-                            sum_wmd = sum_wmd + Number(job.amount);
-                            break;
-                            case "sax":
-                            sum_sax = sum_sax + Number(job.amount);
-                            break;
-                            case "sil":
-                            sum_sil = sum_sil + Number(job.amount);
-                            break;
-                            case "pap":
-                            sum_pap = sum_pap + Number(job.amount);
-                            break;
-                            case "prm":
-                            sum_prm = sum_prm + Number(job.amount);
-                            break;
-                            case "hee":
-                            sum_hee = sum_hee + Number(job.amount);
-                            break;
+                        producerSumlist.sort((a, b) => {
+                            if (b.sum > a.sum) {
+                            return 1;
                             }
+                            else if (b.sum < a.sum) {
+                            return -1;
+                            }
+                            return 0;
+                        });
+
                         }
+                        
                     )
 
-                    jobList = fbJobs;
-                    summe_chr = sum_chr;
-                    summe_doe = sum_doe;
-                    summe_pwd = sum_pwd;
-                    summe_wmd = sum_wmd;
-                    summe_sax = sum_sax;
-                    summe_sil = sum_sil;
-                    summe_pap = sum_pap;
-                    summe_prm = sum_prm;
-                    summe_hee = sum_hee;
-                    }
-                )
-                console.log(index);
-                
+
+                    
+                jobList = fbJobs
+            }
+        )
+
+
 </script>
+
 
 <div class="job-list">
     <h1>Auswertung nach Produzenten</h1>
     <br />
-    <h3>Chromik: <span>{summe_chr} Euro</span></h3>
-    <h3>Doerte: <span>{summe_doe} Euro</span></h3>
-    <h3>Printworld: <span>{summe_pwd} Euro</span></h3>
-    <h3>wirMachenDruck: <span>{summe_wmd} Euro</span></h3>
-    <h3>Saxoprint: <span>{summe_sax} Euro</span></h3>
-    <h3>Silberdruck: <span>{summe_sil} Euro</span></h3>
-    <h3>Pape: <span>{summe_pap} Euro</span></h3>
-    <h3>Primus: <span>{summe_prm} Euro</span></h3>
-    <h3>Heenemann: <span>{summe_hee} Euro</span></h3>
-    <br>
-    <p>{producerSumlist[0].name} {producerSumlist[0].sum}</p>
-    <br>
-    
-    
-
-   
+    <ul>
+	    {#each producerSumlist as item, index (item.id) }
+            <div transition:fade={{ delay: index * 50, duration: 100 }} class="producer" style="width:{(item.sum/producerSumlist[0].sum)*100}%;">
+		    <li>{item.name} {item.sum}</li>
+        </div>
+	    {/each}
+    </ul>
 </div>
 
 <style>
     .job-list{
         display: inline-block;
+        width: 100%;
     }
 
+    .producer {
+        display:block;
+        background-color: darkgray;
+        margin: 1rem;
+        height: 3rem;
+        position: relative;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    li {
+        position: absolute; 
+        top: 50%; 
+        transform: translateY(-50%);
+        margin-left: 1rem;
+    }
 </style>
